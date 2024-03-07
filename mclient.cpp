76e7b891e76105a420c47045d9ce0b8b6d2b6e09
VadimS <vadim.sukhomud@gmail.com>
2024-03-07 21:46:36 +0300

    Polishing

--- mclient.cpp
+++ mclient.cpp
@@ -1,7 +1,9 @@
-#include "SimpleIDL.hh"  // Подключите сгенерированный заголовочный файл для интерфейса
+#include "SimpleIDL.hh"
 #include <omniORB4/CORBA.h>
 #include <iostream>
 #include <fstream>
+#include <vector>
+#include <sstream>
 
 int main(int argc, char** argv) {
     /*if (argc != 2) {
@@ -10,41 +12,74 @@ int main(int argc, char** argv) {
     }*/
     try {
         std::string ior, tmp;
-        std::ifstream MyReadFile("ior.txt");
-        while (getline (MyReadFile, tmp)) {
+        std::string iorFilename = "ior.txt";
+        std::ifstream MyReadFile(iorFilename);
+
+        if (!MyReadFile.is_open()) 
+        {
+            std::cerr << "No IOR file " << iorFilename << std::endl;
+            return 1;
+        }
+
+        while (getline (MyReadFile, tmp)) 
+        {
             ior += tmp;
         }
 
+        MyReadFile.close();
+
         // Инициализация ORB
         CORBA::ORB_var orb = CORBA::ORB_init(argc, argv);
-
         // Преобразование строки IOR в объектную ссылку
         CORBA::Object_var obj = orb->string_to_object(ior.c_str());
-
         // Сужение ссылки к типу MyInterface
         MyInterface_var SimpleIDL = MyInterface::_narrow(obj);
 
-        if (CORBA::is_nil(SimpleIDL)) {
+        if (CORBA::is_nil(SimpleIDL)) 
+        {
             std::cerr << "Can't narrow reference to type MyInterface (invalid IOR?)" << std::endl;
             return 1;
         }
 
-        // Вызов метода Summa
-
         LongArray arrayWithNumbers;
-        arrayWithNumbers.length(5);
-        arrayWithNumbers[0] = 10;
-        arrayWithNumbers[1] = 0;
-        arrayWithNumbers[2] = 3;
-        arrayWithNumbers[3] = 20;
-        arrayWithNumbers[4] = 100;
+
+        std::string NumbersFilename = "numbers.txt";
+        std::ifstream NumbersFile(NumbersFilename);
+
+        if (!NumbersFile.is_open()) 
+        {
+            std::cerr << "Cannot open file " << NumbersFilename << std::endl;
+            return 1;
+        }
+
+        std::vector<long> numbersv;
+        tmp = "";
+
+        while (std::getline(NumbersFile, tmp)) 
+        {
+            std::stringstream ss(tmp);
+            std::string number;
+
+            while (std::getline(ss, number, ',')) {
+                long num = std::stol(number);
+                numbersv.push_back(num);
+            }
+        }
+
+        arrayWithNumbers.length(numbersv.size());
+
+        for (auto i = 0; i < numbersv.size(); ++i) {
+            arrayWithNumbers[i] = numbersv[i];
+        }   
 
         CORBA::Long result = SimpleIDL->Summa(arrayWithNumbers);
         std::cout << "The sum is: " << result << std::endl;
 
         // Разрушение ORB
         orb->destroy();
-    } catch (const CORBA::Exception& ex) {
+
+    } catch (const CORBA::Exception& ex) 
+    {
         std::cerr << "Caught CORBA::Exception." << std::endl;
         return 1;
     }
--- mserver.cpp
+++ mserver.cpp
@@ -10,15 +10,11 @@ int main(int argc, char** argv) {
         PortableServer::POA_var poa = PortableServer::POA::_narrow(obj);
         PortableServer::POAManager_var poa_manager = poa->the_POAManager();
 
-        SumCalculationImpl* myServant = new SumCalculationImpl; // Изменено здесь
+        SumCalculationImpl* myServant = new SumCalculationImpl;
         PortableServer::ObjectId_var myServantId = poa->activate_object(myServant);
         CORBA::Object_var myServantRef = poa->id_to_reference(myServantId);
 
-        // Нет необходимости явно вызывать _remove_ref для myServant
-        // Так как POA управляет жизненным циклом сервантов
-
         CORBA::String_var ior = orb->object_to_string(myServantRef);
-        std::cout << "IOR: " << ior.in() << std::endl;
 
         std::ofstream myfile("ior.txt");
         myfile << ior;
@@ -26,58 +22,11 @@ int main(int argc, char** argv) {
 
         poa_manager->activate();
         orb->run();
-    } catch (const CORBA::Exception& ex) {
+    } 
+    catch (const CORBA::Exception& ex) 
+    {
         std::cerr << "Caught CORBA::Exception." << std::endl;
         return 1;
     }
     return 0;
 }
-
-/*
-int main(int argc, char** argv) {
-    try {
-        // Инициализация ORB
-        CORBA::ORB_var orb = CORBA::ORB_init(argc, argv);
-
-        // Получение ссылки на корневой POA
-        CORBA::Object_var obj = orb->resolve_initial_references("RootPOA");
-        PortableServer::POA_var poa = PortableServer::POA::_narrow(obj);
-        PortableServer::POAManager_var poa_manager = poa->the_POAManager();
-
-        // Создание серванта
-        PortableServer::Servant_var<SumCalculationImpl> myInterfaceImpl = new SumCalculationImpl();
-
-        PortableServer::ObjectId_var myServantId = poa->activate_object(myServant);
-        CORBA::Object_var myServantRef = poa->id_to_reference(myServantId);
-
-        // Явное уменьшение счетчика ссылок на сервант, так как POA управляет его жизненным циклом
-        myServant->_remove_ref();
-
-        // Получение IOR (Interoperable Object Reference)
-        CORBA::String_var ior = orb->object_to_string(myServantRef);
-
-        // Активация POA Manager
-        //poa_manager->activate();
-
-        // Получение ссылки на объект
-        //CORBA::Object_var myInterface_obj = myInterfaceImpl->_this();
-
-        // Вывод строки объектной ссылки в формате IOR
-        //CORBA::String_var ior = orb->object_to_string(myInterface_obj);
-
-        std::ofstream myfile;
-        myfile.open ("ior.txt");
-        myfile << ior;
-        myfile.close();
-
-        std::cout << "IOR: " << ior << std::endl;
-
-        // Запуск ORB
-        orb->run();
-    } catch (const CORBA::Exception& ex) {
-        std::cerr << "Caught CORBA::Exception." << std::endl;
-        return 1;
-    }
-    return 0;
-}
-*/
